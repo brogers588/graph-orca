f74d86a0f23fb3b6443f135176cd06015016d304
Samuel Poulton <samuel.poulton@jupiterone.com>
2022-06-16 11:17:38 -0600

    Update Findings test - Update test to pass with mapped relationship

--- src/steps/findings/index.test.ts
+++ src/steps/findings/index.test.ts
@@ -1,7 +1,12 @@
 import { executeStepWithDependencies } from '@jupiterone/integration-sdk-testing';
 import { buildStepTestConfigForStep } from '../../../test/config';
 import { Recording, setupProjectRecording } from '../../../test/recording';
-import { Steps } from '../constants';
+import {
+  Entities,
+  MappedRelationships,
+  Relationships,
+  Steps,
+} from '../constants';
 
 // See test/README.md for details
 let recording: Recording;
@@ -9,13 +14,33 @@ afterEach(async () => {
   await recording.stop();
 });
 
-test.skip('fetch-findings', async () => {
+test('fetch-findings', async () => {
   recording = setupProjectRecording({
     directory: __dirname,
     name: 'fetch-findings',
   });
 
-  const stepConfig = buildStepTestConfigForStep(Steps.USERS);
-  const stepResult = await executeStepWithDependencies(stepConfig);
-  expect(stepResult).toMatchStepMetadata(stepConfig);
+  const stepConfig = buildStepTestConfigForStep(Steps.FINDINGS);
+  const { collectedEntities, collectedRelationships, encounteredTypes } =
+    await executeStepWithDependencies(stepConfig);
+
+  expect(encounteredTypes).toMatchSnapshot();
+  expect(
+    collectedEntities.some((e) => e._type === Entities.FINDING._type),
+  ).toBeTruthy();
+  expect(
+    collectedRelationships.some(
+      (e) => e._type === Relationships.ACCOUNT_HAS_FINDING._type,
+    ),
+  ).toBeTruthy();
+  expect(
+    collectedRelationships.some(
+      (e) => e._type === Relationships.ASSET_HAS_FINDING._type,
+    ),
+  ).toBeTruthy();
+  expect(
+    collectedRelationships.some(
+      (e) => e._type === MappedRelationships.FINDING_IS_CVE._type,
+    ),
+  ).toBeTruthy();
 });
